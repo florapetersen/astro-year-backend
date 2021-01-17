1a4f841158ecb4cb897a435e78fce0762226cd2b
florapetersen <floramarina@comcast.net>
2021-01-17 12:35:10 -0800

    clean up seeds.db

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,8 @@
 class ProductSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name, :description, :link, :image_src, :category_id, :active
+
+  attributes :category_name do |object|
+    object.category.name
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,20 @@
 
 user = User.first
 
-loungewear = Category.create(name: "Loungewear")
-office = Category.create(name: "Office")
-activewear = Category.create(name: "Activewear")
-shoes = Category.create(name: "Shoes")
-outerwear = Category.create(name: "Outerwear")
-skincare = Category.create(name: "Skincare")
-makeup = Category.create(name: "Makeup")
-tops = Category.create(name: "Tops")
-bottoms = Category.create(name: "Bottoms")
-denim = Category.create(name: "Denim")
-home_goods = Category.create(name: "Home Goods")
-kitchenware = Category.create(name: "Kitchenware")
-accessories = Category.create(name: "Accessories")
+loungewear = Category.find_or_create_by(name: "Loungewear")
+office = Category.find_or_create_by(name: "Office")
+activewear = Category.find_or_create_by(name: "Activewear")
+shoes = Category.find_or_create_by(name: "Shoes")
+outerwear = Category.find_or_create_by(name: "Outerwear")
+skincare = Category.find_or_create_by(name: "Skincare")
+makeup = Category.find_or_create_by(name: "Makeup")
+tops = Category.find_or_create_by(name: "Tops")
+bottoms = Category.find_or_create_by(name: "Bottoms")
+denim = Category.find_or_create_by(name: "Denim")
+home_goods = Category.find_or_create_by(name: "Home Goods")
+kitchenware = Category.find_or_create_by(name: "Kitchenware")
+accessories = Category.find_or_create_by(name: "Accessories")
 
-girlfriend_collective_classic_hoodie = user.products.find_or_create_by(name: "Girlfriend Collective Classic Hoodie", description: "This is the perfect hoodie! Cozy and simple. Comes in 5 colors and sizes XXS - 6XL. Also love their matching joggers for a complete set.", link: "https://www.girlfriend.com/collections/sweatshirts/products/canyon-classic-hoodie", category_id: loungewear.id)
-girlfriend_collective_classic_joggers = user.products.find_or_create_by(name: "Girlfriend Collective Classic Joggers", description: "I love to combine these joggers with the Girlfriend Collective Classic Hoodie for a simple, cozy sweatsuit. Come in sizes XXS - 6XL.", link: "https://www.girlfriend.com/products/canyon-classic-jogger", category_id: loungewear.id)
-leuchtturm_weekly_planner = user.products.find_or_create_by(name: "Leuchtturm1917 Weekly Planner", description: "These are my absolute favorite planners. I usually go for the medium size, softcover. It's flexible and easy to carry in your day bag. They come in about a million colors and have a ton of blank or lined pages for note-taking.", link: "https://www.leuchtturm1917.us/weekly-planner-notebook-softcover-2021-softcover-english.html", category_id: office.id)
+
 
 
